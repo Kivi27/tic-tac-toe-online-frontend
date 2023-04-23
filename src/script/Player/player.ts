13fcd7d61adf33752b1724170c1ff5a650d476cf
Rostyslav Liashenko <54037162+Kivi27@users.noreply.github.com>
2023-04-23 18:56:24 +0300

    feat: loading and display room card

---
+++ src/script/Player/player.ts
@@ -0,0 +1,10 @@
+
+export class Player {
+    private readonly id: string;
+    private readonly symbol: string;
+
+    constructor(id, symbol) {
+        this.id = id;
+        this.symbol = symbol;
+    }
+}
--- src/script/main.ts
+++ src/script/main.ts
@@ -1,5 +1,26 @@
 import { io } from 'socket.io-client'
+import { Room } from './Room/room';
 
 const socket = io('http://localhost:3000');
 
-socket.emit('hello', 'hello1');
+socket.on('initial_room', (dtoRooms) => {
+    const roomsContainer = document.querySelector('.rooms');
+
+    if (!roomsContainer) return;
+
+    const rooms:Room[] = initRooms(dtoRooms);
+
+    rooms.forEach((room: Room) => {
+        roomsContainer.append(room.getView());
+    });
+});
+
+function initRooms(dtoRooms): Room[] {
+    const rooms: Room[] = [];
+    dtoRooms.forEach((dtoRoom) => {
+        const room = new Room(dtoRoom.id, dtoRoom.name, dtoRoom.players);
+        rooms.push(room);
+    });
+
+    return rooms;
+}
